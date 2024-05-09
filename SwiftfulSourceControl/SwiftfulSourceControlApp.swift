a10b184e2a4b88afe1a66b89c2230bc00014e6b6
Aeshma <madhushanka2727@gmail.com>
2024-05-09 21:59:18 +0530

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Malith Madhushanka on 2024-05-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
