baf551234054d7c5af9229066e2bc520ce76aeea
Aeshma <madhushanka2727@gmail.com>
2024-05-13 20:16:25 +0530

    [Feature] Add explore view

---
+++ SwiftfulSourceControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Malith Madhushanka on 2024-05-13.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
